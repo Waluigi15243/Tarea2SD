7f326eaf3f039e9dae5301a69c84ae33bf8c2a20
Waluigi15243 <81125222+Waluigi15243@users.noreply.github.com>
2024-05-28 09:42:16 -0400

    Update procesamiento.py

--- procesamiento.py
+++ procesamiento.py
@@ -3,28 +3,30 @@ import json
 from kafka import KafkaProducer
 from kafka import KafkaConsumer
 
-topic = "detalle_compra"
+topic = "compra_detalles"
 topicNotif = "compra_confirmada"
 
-consumer = KafkaConsumer(topic, bootstrap_servers="localhost:9092")
+consumer = KafkaConsumer(topic, bootstrap_servers="localhost:29092")
+producer = KafkaProducer(bootstrap_servers="localhost:29092")
 
 print("Buscando transacciones...")
 while True:
     for message in consumer:
+        print("Recibiendo transaccion...")
         consumed_order = json.loads(message.value.decode())
         print(consumed_order)
-        orderid = consumed_order["orderid"]
-        gameid = consumed_order["gameid"]
-        price = consumed_order["price"]
-        name = consumed_order["name"]
-        usermail = consumed_order["usermail"]
-        data = {
-            "orderid": orderid,
-            "gameid": gameid,
-            "nombre": name,
-            "price": price,
-            "usermail": f"{userid}@correo.com",
-            "estado": "recibido",
-        }
-        print("Transaccion recibida!")
-        producer.send(topicNotif, json.dumps(data).encode("utf-8"))
+        #orderid = consumed_order["orderid"]
+        #gameid = consumed_order["gameid"]
+        #price = consumed_order["price"]
+        #name = consumed_order["name"]
+        #usermail = consumed_order["usermail"]
+        #data = {
+        #    "orderid": orderid,
+        #    "gameid": gameid,
+        #    "nombre": name,
+        #    "price": price,
+        #    "usermail": f"{userid}@correo.com",
+        #    "estado": "recibido",
+        #}
+        #print("Transaccion recibida!")
+        #producer.send(topicNotif, json.dumps(data).encode("utf-8"))
