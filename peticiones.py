2b9be86eb9815075ae8ef924b31a6c07ea3811a4
Waluigi15243 <81125222+Waluigi15243@users.noreply.github.com>
2024-05-29 12:33:01 -0400

    Update peticiones.py

--- peticiones.py
+++ peticiones.py
@@ -7,6 +7,7 @@ import csv
 
 app = Flask(__name__)
 topic = "compra_detalles"
+transaction = 0
 
 producer = KafkaProducer(bootstrap_servers="localhost:29092")
 @app.route('/', methods=['POST'])
@@ -14,18 +15,19 @@ def sendOrder():
     with open("steamgames.csv") as archivo:
         i = int(request.form['value'])
         data = csv.reader(archivo)
-        for _ in range(i+1):
+        for _ in range(i):
             next(data)
         row = next(data)
         number = randrange(2000)
+        transaction += 1
         order = {
-            "orderid": number,
+            "orderid": transaction,
             "gameid": row[0],
             "name": row[1],
             "price": row[2],
             "usermail": f"{number}@correo.com",
         }
-        print(order)
+        print(order + "\n")
         producer.send(topic, json.dumps(order).encode("utf-8"))
         print("Transaccion enviada!")
         return "Transaccion enviada!"
