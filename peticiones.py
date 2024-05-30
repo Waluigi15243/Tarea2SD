b4e70d5ed660fd8e3e776a62327071bbea271432
Waluigi15243 <81125222+Waluigi15243@users.noreply.github.com>
2024-05-30 11:35:18 -0400

    Update peticiones.py

--- peticiones.py
+++ peticiones.py
@@ -24,7 +24,7 @@ def sendOrder():
             "gameid": row[0],
             "name": row[1],
             "price": row[2],
-            "usermail": "",
+            "usermail": "bosolov589@jzexport.com",
         }
         print(order)
         producer.send(topic, json.dumps(order).encode("utf-8"))
